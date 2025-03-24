9e7e9555880c398e1d02c86183d643d752032fb7
Michael Buhler <michael@michaelbuhler.com>
2025-03-24 13:25:18 +0700

    feat(loader): add browser esm to esbuild config

--- loader/esbuild.js
+++ loader/esbuild.js
@@ -1,9 +1,32 @@
 import * as esbuild from 'esbuild'
+import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill'
 
+
+// CJS
 await esbuild.build({
   entryPoints: ['src/index.cjs'],
   platform: 'node',
   format: 'cjs',
   bundle: true,
   outfile: 'dist/index.cjs'
+})
+
+// Browser ESM
+await esbuild.build({
+  entryPoints: ['src/index.cjs'],
+  plugins: [
+    nodeModulesPolyfillPlugin({
+      modules: {
+        buffer: true,
+        child_process: 'empty',
+        fs: 'empty',
+        path: 'empty'
+      }
+    })
+  ],
+  platform: 'browser',
+  format: 'esm',
+  bundle: true,
+  minify: true,
+  outfile: 'dist/browser.js'
 })
